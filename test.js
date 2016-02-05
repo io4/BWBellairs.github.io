ed7dbd1c26cecedfa168209877e328b792db931e
io <io4@users.noreply.github.com>
2016-02-05 16:13:07 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-for (var i = 0; i <= 50; i++){
-  document.getElementById("test").style.width = i;
-  setTimeout(1000) //wait ten seconds before continuing
-};
+var i = 0;
+var ival = setInterval(function(){document.getElementById("test").style.width = i;i++}, 100);
+setTimeout(function(){clearInterval(ival)}, 100000)
